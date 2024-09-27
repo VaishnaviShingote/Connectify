7dcf4a462e7c5ed964bec66e851bca72fdece3bc
VaishnaviShingote <107314843+VaishnaviShingote@users.noreply.github.com>
2024-09-27 15:32:36 +0530

    remove server

--- server/routes/AuthRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { signup } from "../controller/authController.js";
-
-const authRoutes = Router();
-
-authRoutes.post("/signup", signup);
-
-export default authRoutes;
